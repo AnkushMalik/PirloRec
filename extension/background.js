3941e7155a27f1b7f8ffefbf69b219b7505190ff
Ankush Malik <ankushmalik631@gmail.com>
2019-07-28 14:30:53 +0530

    Add click and input tracking
    will send data to

--- extension/background.js
+++ extension/background.js
@@ -3,14 +3,25 @@ checker = 0;
 chrome.browserAction.onClicked.addListener(function(tab) {
   if (checker % 2 == 0) {
     window.open("https://www.google.com", "_blank");
+
     chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
       if (changeInfo.status == "complete" && tab.active) {
         chrome.tabs.sendMessage(tab.id, "Start");
       }
     });
-    chrome.tabs.onUpdated.addListener();
   } else {
     chrome.tabs.sendMessage(tab.id, "Stop");
   }
   checker++;
+});
+
+chrome.runtime.onMessage.addListener(function(request) {
+  let data = request;
+  $.ajax({
+    url: "http://localhost:3000/record_action",
+    dataType: "json",
+    type: "POST",
+    data: data
+  });
+  console.log(data);
 });
--- extension/content.js
+++ extension/content.js
@@ -1,4 +1,4 @@
-events_to_record = ["click scroll submit keypres"]; //all events to map
+events_to_record = ["click scroll keyup"]; //all events to map
 
 chrome.runtime.onMessage.addListener(controller);
 
@@ -13,10 +13,9 @@ function controller(message, sender, sendResponse) {
 }
 
 function run() {
-  $("body").on("click scroll submit", function(e) {
+  $("body").on("click scroll input", function(e) {
     element_path = dompath(e.target);
-    event_type = e.type;
-    handle_recording(event_type, element_path);
+    handle_recording(e, element_path);
   });
 }
 
@@ -32,4 +31,51 @@ function dompath(element) {
   return path;
 }
 
-function handle_recording() {}
+function handle_recording(e, path) {
+  console.log(path);
+  switch (e.type) {
+    case "click":
+      chrome.runtime.sendMessage({ type: "click", path: path });
+      break;
+    case "input":
+      chrome.runtime.sendMessage({
+        type: "input",
+        path: path,
+        value: e.target.value
+      });
+      break;
+    default:
+      console.log("tracking only click and data input only as of now");
+      break;
+  }
+}
+
+// function handle_recording(e, path) {
+//   switch (e.type) {
+//     case "click":
+//       $.ajax({
+//         url: "http://localhost:8888/record_action",
+//         dataType: "json",
+//         type: "POST",
+//         data: JSON.stringify({ path: path, action: e.type })
+//       });
+//       console.log("click called");
+
+//       break;
+//     case "keyup":
+//       $(e.target)
+//         .closest("form")
+//         .off("submit")
+//         .on("submit", function() {
+//           input = e.target.val();
+//           $.ajax({
+//             url: "http://localhost:8888/type_Action",
+//             dataType: "json",
+//             type: "POST",
+//             data: JSON.stringify({ path: path, action: "typed", text: text })
+//           });
+//           console.log("keyup called");
+//         });
+//       break;
+//   }
+// }
