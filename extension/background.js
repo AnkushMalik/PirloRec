1d0de875a24372568345987008ebeaa71fc29fb2
Ankush Malik <ankushmalik631@gmail.com>
2019-07-28 09:42:28 +0530

    Add some functions
    created skeleton, ie added some important function

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,16 @@
-chrome.browserAction.onClicked.addListener(function() {
-  console.log("yes it is working!");
+checker = 0;
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  if (checker % 2 == 0) {
+    window.open("https://www.google.com", "_blank");
+    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+      if (changeInfo.status == "complete" && tab.active) {
+        chrome.tabs.sendMessage(tab.id, "Start");
+      }
+    });
+    chrome.tabs.onUpdated.addListener();
+  } else {
+    chrome.tabs.sendMessage(tab.id, "Stop");
+  }
+  checker++;
 });
--- extension/content.js
+++ extension/content.js
@@ -1,1 +1,35 @@
-console.log("is it working?");
+events_to_record = ["click scroll submit keypres"]; //all events to map
+
+chrome.runtime.onMessage.addListener(controller);
+
+function controller(message, sender, sendResponse) {
+  console.log(message);
+
+  if (message == "Start") {
+    run();
+  } else {
+    console.log("Pirlo recording stopped");
+  }
+}
+
+function run() {
+  $("body").on("click scroll submit", function(e) {
+    element_path = dompath(e.target);
+    event_type = e.type;
+    handle_recording(event_type, element_path);
+  });
+}
+
+function dompath(element) {
+  var path = "";
+  for (; element && element.nodeType == 1; element = element.parentNode) {
+    var inner = $(element).children().length == 0 ? $(element).text() : "";
+    var eleSelector =
+      element.tagName.toLowerCase() +
+      (inner.length > 0 ? ":contains('" + inner + "')" : "");
+    path = " " + eleSelector + path;
+  }
+  return path;
+}
+
+function handle_recording() {}
